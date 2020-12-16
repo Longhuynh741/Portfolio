d2719ae9140f38d609e20826593c91b6f234f735
Long Huynh <Longhuynh741@yahoo.com>
2020-12-15 22:00:56 -0500

    updated projects page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
+require("./script.css");
 
 export default class Projects extends Component {
   render() {
     return (
       <div>
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).{" "}
+            <div className="container">
+            <h1> My Projects</h1>
+          <div className="row projectContainer">
+            <div className="col-md-8">
+              <img
+                className="verdant"
+                src="https://user-images.githubusercontent.com/69770137/99292540-5758f400-280f-11eb-93f7-c3299670bd29.png"
+              />
+            </div>
+                    <div className="col-md-4"> <h1>Verdant-Green Thumbs</h1>
+                     <br />
+                        Full MERN Stack project designed for plant enthusiasts to explore the world of plants! Users can create a profile, research plant articles, and connect with other enthusiasts through our web application. 
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+                       <h1> The Wedding Pot </h1>
+                                     <br /> 
+                        FullStack web application designed for wedding events. A easy to use platform for wedding planners to plan their event and connect with vendors that they could potentially hire!
+            </div>
+                    <div className="col-md-8">
+                    <img
+                className="verdant"
+                src="https://github.com/Longhuynh741/project-2/blob/main/images/Home%20Page.png?raw=true"
+              />    </div>
+          </div>
+          <div className="row">
+            <div className="col-md-8">
+              <img
+                className="verdant"
+                src="https://github.com/Napica/Toned-Tribe/raw/master/asset/images/HomePage2.png"
+              />
+            </div>
+                    <div className="col-md-4">
+                        <h1> Toned Tribe </h1>
+            <br />
+                API-based web application designed for users to search and locate gyms and marketplaces in their desired area.        </div>
+          </div>
+        </div>
       </div>
     );
   }
