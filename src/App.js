e76d199d8586bf5ef385135dfe22c0acc53c23f3
Long Huynh <Longhuynh741@yahoo.com>
2020-12-16 02:43:49 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar/index";
 import Projects from "./components/Projects";
 import HeroImg from "./components/HeroImg";
 import Profile from "./components/Profile";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Profile />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+require("./script.css");
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                
+                <h4 id ="footerText"> Long Huynh © 2020</h4>
+
+            </footer>
+        )
+    }
+}
